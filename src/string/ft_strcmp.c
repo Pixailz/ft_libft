2089a4ed98c0b3ffb926be25d1a62890781b2d4b
Pixailz <pixailz1@gmail.com>
2022-09-01 02:33:32 +0200

    :x: src/string/ft_str(ncmp|cmp) - fixing stuff ...

--- src/string/ft_strcmp.c
+++ src/string/ft_strcmp.c
@@ -25,7 +25,10 @@
  */
 int	ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1++ == *s2)
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
 		s2++;
+	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
--- src/string/ft_strncmp.c
+++ src/string/ft_strncmp.c
@@ -28,7 +28,10 @@ int	ft_strncmp(char *s1, char *s2, t_size n)
 {
 	if (!n)
 		return (0);
-	while (*s1 && *s2 && --n && *s1++ == *s2)
+	while (*s1 && *s2 && --n && *s1 == *s2)
+	{
+		s1++;
 		s2++;
+	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
