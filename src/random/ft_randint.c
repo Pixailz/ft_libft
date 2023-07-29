d49fc5103f6560006c3a0bc219cb49090320c2df
Pixailz <pixailz1@gmail.com>
2023-07-29 21:05:58 +0200

    [random:ft_randint] improvement

--- src/random/ft_randint.c
+++ src/random/ft_randint.c
@@ -14,24 +14,11 @@
 
 t_int64	ft_randint(int start, int end)
 {
-	int				file;
-	t_int64			range;
-	t_int64			sum;
-	t_int64			counter;
-	unsigned char	*buff;
+	int		file;
+	char	buff[4];
 
-	range = end - start + 1;
-	if (range < 0)
-		return (end + start + 1);
-	range = (range / 0xff) + 1;
 	file = open("/dev/urandom", O_RDONLY);
-	buff = ft_calloc(sizeof(char), range);
-	read(file, buff, range);
+	read(file, buff, 4);
 	close(file);
-	sum = 0;
-	counter = 0;
-	while (counter++ < range)
-		sum += buff[counter - 1];
-	free(buff);
-	return ((sum % (end - start + 1)) + start);
+	return ((int)(*((unsigned int*)buff) % (end - start)) + start);
 }
