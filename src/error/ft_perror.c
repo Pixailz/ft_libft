677c9d8eddccc1b73791ea3113c70ff23124db4b
Pixailz <pixailz1@gmail.com>
2023-07-30 06:22:44 +0200

    ...

--- src/error/ft_perror.c
+++ src/error/ft_perror.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:43:45 by brda-sil          #+#    #+#             */
-/*   Updated: 2023/07/30 06:21:32 by brda-sil         ###   ########.fr       */
+/*   Updated: 2023/07/30 06:22:35 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@ int	ft_perror(char *msg)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putstr_fd(msg, 2);
+	return (1);
 }
