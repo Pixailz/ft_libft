669ed26602db831d32db3139fc194f21775cd3c9
Pixailz <Pixailz1@gmail.com>
2023-11-21 05:34:27 +0100

    norminette

--- src/network/ipv4/ft_ltoaddr.c
+++ src/network/ipv4/ft_ltoaddr.c
@@ -6,17 +6,17 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 04:02:43 by brda-sil          #+#    #+#             */
-/*   Updated: 2023/11/21 05:24:04 by brda-sil         ###   ########.fr       */
+/*   Updated: 2023/11/21 05:34:20 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_network/ipv4.h"
 
-struct sockaddr ft_ltoaddr(t_int4 ip)
+struct sockaddr	ft_ltoaddr(t_int4 ip)
 {
 	struct sockaddr_in	addr;
 
 	ft_memset(&addr, 0, sizeof(struct sockaddr_in));
 	addr.sin_addr.s_addr = ip;
-	return *((struct sockaddr *)&addr);
+	return (*((struct sockaddr *)&addr));
 }
